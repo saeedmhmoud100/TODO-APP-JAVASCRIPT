1b35b78d06074e6887b7e2b18a2d789ef66308f6
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-09-10 14:18:14 +0200

    created the project(initial)

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 document.querySelector("[type='submit']").onclick = _ => addTask();
 
+function createTask(text) {
+  let task = document.createElement("div");
+  task.setAttribute("class", "task");
+  let h3 = document.createElement("h3");
+  h3.innerText = text;
+  task.appendChild(h3);
+  let button = document.createElement("button");
+  button.innerText = "Delete";
+  button.onclick = removeTask;
+  task.appendChild(button);
+  document.querySelector(".tasks").appendChild(task);
+}
+
 function addTask() {
   let text = document.querySelector('[type="text"]');
   if (text.value) {
-    let task = document.createElement("div");
-    task.setAttribute("class", "task");
-    let h3 = document.createElement("h3");
-    h3.innerText = text.value;
-    task.appendChild(h3);
-    let button = document.createElement("button");
-    button.innerText = "Delete";
-    button.onclick = removeTask;
-    task.appendChild(button);
-    document.querySelector(".tasks").appendChild(task);
-
+    createTask(text.value);
     if (localStorage.tasks) {
       let tasks = [localStorage["tasks"], text.value];
       localStorage.tasks = tasks;
@@ -25,6 +28,19 @@ function addTask() {
 }
 
 function removeTask(e) {
-  
+  let text = e.target.parentElement.children[0].innerText;
+  //   console.log(localStorage["tasks"].contains("," + text));
+  localStorage.setItem("tasks", localStorage["tasks"].replace(`,${text}`, ""));
+  localStorage.setItem("tasks", localStorage["tasks"].replace(`${text}`, ""));
   e.target.parentElement.remove();
+
+  localStorage["tasks"] == "" ? localStorage.removeItem("tasks") : true;
 }
+
+window.onload = _ => {
+  localStorage["tasks"]
+    ? localStorage["tasks"]
+        .split(",")
+        .map(n => (n != "" ? createTask(n) : true))
+    : true;
+};
